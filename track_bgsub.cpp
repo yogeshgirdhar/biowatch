dc8db4f63107ebf4efec17496a5b75bf60b55065
Yogesh Girdhar <yogesh@zeta.gateway.2wire.net>
2010-04-11 15:53:31 -0400

    cleaned up mathematica code

--- track_bgsub.cpp
+++ track_bgsub.cpp
@@ -38,6 +38,7 @@ int main(int argc, char* argv[]){
     cerr<<"Usage: "<<argv[0]<<" <videofilename>"<<endl;
     exit(0);
   }
+  int MAX_ANT_AREA=60;
   string filename(argv[1]);
   fs::path pathname(argv[1]);
   std::string dirname  = pathname.parent_path().string();
@@ -54,11 +55,13 @@ int main(int argc, char* argv[]){
   cv::Mat_<float> image_fp(image_tmp.size());
   cv::Mat_<float> image_sans_obs(image_tmp.size());
   cv::Mat_<float> observation(image_tmp.size(),0.0);
+  cv::Mat_<float> observationT(image_tmp.size(),0.0);
   cv::Mat_<float> dilated_observation(image_tmp.size(),0.0);
 
 
   video.grab();
   video.retrieve(image_tmp);
+  cv::imwrite(dirname+"/"+basename+"_bg.jpg",image_tmp);
   image_rgb_last = image_tmp.clone();
   cv::cvtColor(image_tmp, image, CV_BGR2GRAY);
   image.convertTo(image_fp, CV_32F, 1.0/256.0);
@@ -71,21 +74,26 @@ int main(int argc, char* argv[]){
   std::vector<cv::Point2f> points;
   cv::Point2f state;
   cv::RotatedRect rect;
+  cv::RotatedRect last_locked_rect;
 
   std::ofstream out(dirname+"/"+basename+"_data.txt");
+  bool locked=false;
   while(video.grab()){
     count++;
     video.retrieve(image_tmp);
     cv::cvtColor(image_tmp, image, CV_BGR2GRAY);
     image.convertTo(image_fp, CV_32F, 1.0/256.0);
 
-    if(!points.empty()){
-      cv::dilate(observation,observation,cv::Mat());
-    //image_sans_obs =(1.0-observation).mul(image_fp) + observation.mul(accum_image);
+
+    if(!points.empty() && locked){
+      cv::dilate(observationT,observationT,cv::Mat());
+      image_sans_obs =(1.0-observationT).mul(image_fp) + observationT.mul(accum_image);
+      cv::accumulateWeighted(image_sans_obs, accum_image,0.01);
     }
-    cv::accumulateWeighted(image_fp, accum_image,0.01);
-    //    cv::accumulateWeighted(image_sans_obs, accum_image,0.01);
-								      //    accum_image_01 = accum_image *1.0;
+    else{
+      cv::accumulateWeighted(image_fp, accum_image,0.01);
+    }
+
     cv::absdiff(image_fp, accum_image, observation);
 
 
@@ -93,12 +101,12 @@ int main(int argc, char* argv[]){
     cv::Point minLoc, maxLoc;
     cv::minMaxLoc(image_fp,&min, &max, &minLoc, &maxLoc);
     double T = min+(max-min)/4.0;
-    cv::threshold(observation,dilated_observation, T, 1.0, CV_THRESH_BINARY);
+    cv::threshold(observation,observationT, T, 1.0, CV_THRESH_BINARY);
     points.clear();
     state=cv::Point2f(0,0);
     for(int r=0;r<observation.rows;r++){
       for(int c=0;c<observation.cols;c++){
-	if(observation(r,c)==1.0){
+	if(observationT(r,c)==1.0){
 	  points.push_back(cv::Point_<int>(c,r));
 	  //state = state + 
 	}
@@ -108,10 +116,35 @@ int main(int argc, char* argv[]){
     if(points.size()>=1){
       rect = cv::minAreaRect(points);
       rect.angle+=90;
-      cv::ellipse(image_tmp, rect, cv::Scalar(255,0,0));
+
+      if(rect.size.width*rect.size.height < MAX_ANT_AREA)
+	locked=true;
+      else
+	locked=false;
+
+      if(locked){
+	cv::ellipse(image_tmp, rect, cv::Scalar(0,255,0));
+	last_locked_rect=rect;
+      }
+      else{
+	cv::ellipse(image_tmp, last_locked_rect, cv::Scalar(255,0,0));
+	cv::ellipse(image_tmp, rect, cv::Scalar(0,0,255));
+      }
     }
-    out<<rect.center.x<<" "<<(float)image_tmp.rows-rect.center.y<<" "<<rect.size.width<<" "<<rect.size.height<<" "<<rect.angle<<" "<<points.size()<<endl;
-    cerr<<rect.center.x<<" "<<(float)image_tmp.rows-rect.center.y<<" "<<rect.size.width<<" "<<rect.size.height<<" "<<rect.angle<<" "<<points.size()<<endl;
+    out<<count<<" "
+       <<last_locked_rect.center.x<<" "
+       <<(float)image_tmp.rows-last_locked_rect.center.y<<" "
+       <<last_locked_rect.size.width<<" "
+       <<last_locked_rect.size.height<<" "
+       <<last_locked_rect.angle<<" "
+       <<(int)locked<<endl;
+    cerr<<count<<" "
+	<<last_locked_rect.center.x<<" "
+	<<(float)image_tmp.rows-last_locked_rect.center.y<<" "
+	<<last_locked_rect.size.width<<" "
+	<<last_locked_rect.size.height<<" "
+	<<last_locked_rect.angle<<" "
+	<<(int)locked<<endl;
 
 
     cv::imshow(filename, image_tmp);	
@@ -124,6 +157,7 @@ int main(int argc, char* argv[]){
     case 27:
       return 0; break;
     case 'b':
+      cerr<<"Writing Background image"<<endl;
       cv::Mat bgimg;
       accum_image.convertTo(bgimg,CV_8U,256.0);
       cv::imwrite(dirname+"/"+basename+"_bg.jpg",bgimg);
