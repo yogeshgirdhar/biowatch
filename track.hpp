a09ebdd44172d93e464c0426f78bcdf5268e461c
Yogesh Girdhar <yogesh@cim.mcgill.ca>
2010-04-19 22:57:58 -0400

    forgot again

---
+++ track.hpp
@@ -0,0 +1,33 @@
+#ifndef TRACK_HPP_239384
+#define TRACK_HPP_239384
+#include <cv.h>
+#include <highgui.h>
+#include <string>
+
+
+
+
+
+
+namespace entropy{
+  class ImageSource{
+  public:
+    ImageSource(const std::string& filename, int subsample=1, double scale=1.0, bool gray=true);
+    ImageSource(int camera_id, int subsample=1, double scale=1.0, bool gray=true);
+    bool grab();
+    bool retrieve(cv::Mat&);
+    bool isOpened();
+    int frame_number();
+  protected:
+    cv::VideoCapture m_video_capture;
+    double m_scale;
+    double m_subsample;
+    int m_num_frames;
+    cv::Mat m_current_frame;
+    int m_curr_framenum;
+    bool m_has_new_frame;
+    bool m_gray;
+  };
+}
+
+#endif
